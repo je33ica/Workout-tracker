91ebe3c37f4eadcb5db9a807597c78d391d4a642
je33ica <je331687@gmail.com>
2021-03-10 21:05:30 +0000

    fixed error- used app instead of router for html route

--- routes/html.js
+++ routes/html.js
@@ -1,18 +1,19 @@
 const router = require("express").Router();
 const path = require("path");
 
-// GET method route
+// GET method routes
 //match up to front end provided- exercise,index,stats
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/exercise", (req, res) => {
+router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-app.get("/stats", (req, res) => {
+router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
+//dont forget to export!
 module.exports = router;
