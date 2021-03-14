15b7a96d4b93998ad9e9832928e0595c1c548532
je33ica <je331687@gmail.com>
2021-03-14 11:38:40 +0000

    fixed db issues and added aggregation to fields

--- models/workout.js
+++ models/workout.js
@@ -1,96 +1,42 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-//const { Schema } = mongoose;
+//const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const workoutSchema = new Schema({
-  day: { type: Date, default: Date.now },
+  day: { type: Date, default: () => new Date() },
   exercises: [
     {
       type: {
         type: String,
-        //enum: specifies the set of allowed values for the field.
-        // enum: ["Resistance", "Cardio"],
-        //don't think i will need thes due to the front end already provided
-        // lowercase: true,
-        // trim: true,
-        // required: [true, "An exercise type is required"],
+
+        trim: true,
+        required: "An exercise type is required",
       },
       name: {
         type: String,
-        // lowercase: true,
-        // trim: true,
-        // required: [true, "What is the name of the exercise?"],
+        trim: true,
+        required: "An exercise type is required",
       },
       duration: {
         type: Number,
-        // min: 1,
-        // max: 60,
-        // required: true,
       },
       weight: {
         type: Number,
-        // min: 0,
-        // max: 1000,
       },
       reps: {
         type: Number,
-        // min: 1,
-        // max: 100,
       },
       sets: {
         type: Number,
-        // min: 1,
-        // max: 50,
       },
       distance: {
         type: Number,
-        // min: 1,
-        // max: 1000,
       },
     },
   ],
-
-  //If you want the virtual field to be displayed on client side,
-  // then set {virtuals: true} for toObject and toJSON in schema
-  // },
-  // { toJSON: { virtuals: true } }
 });
 
-// //mdn reduce, array.map
-// workoutSchema.virtual("totalDuration").get(function () {
-//   const duration = this.exercises.reduce((acc, cur) => {
-//     return acc + cur.duration;
-//   }, 0);
-
-//   return duration;
-// });
-
-// //acc = accumulator, cur = current value
-// workoutSchema.virtual("totalDistance").get(function () {
-//   const distance = this.exercises.reduce((acc, cur) => {
-//     if (cur.age) {
-//       return acc + cur.distance;
-//     }
-
-//     return acc;
-//   }, 0);
-
-//   return distance;
-// });
-
-//the total duration was not showing so we set a virtual field which will create the
-//duration but not send it or store it in the db
-
-//workoutSchema.virtual("totalDuration").get(function () {});
-
-// adds a dynamically-created property to schema
-// workoutSchema.virtual("totalDuration").get(function () {
-//   // "reduce" array of exercises down to just the sum of their durations
-//   return this.exercises.reduce((total, exercise) => {
-//     return total + exercise.duration;
-//   }, 0);
-// });
-
+console.log("im the wokout");
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
+//dependencies
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
+//local or enviromental for hosting on Heroku
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+//uses morgan- HTTP request logger middleware for node.js
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+//Set up default mongoose connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
